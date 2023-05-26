24681771bdaf7d484d054d5fa7b9cbcecf94c839
Семён Яковченко <shim.valter@yandex.ru>
2023-05-26 18:57:59 +0300

    feature: промежуточное решение 2

--- src/test/java/ru/yandex/practicum/filmorate/UserControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/UserControllerTest.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum.filmorate;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
@@ -11,6 +12,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureTestDatabase
 @AutoConfigureMockMvc
 class UserControllerTest {
 
@@ -67,8 +69,48 @@ class UserControllerTest {
     @Test
     void test4_addFriendWitNotExistUser() throws Exception {
 
-        mockMvc.perform(put("/users/1/friends/2"))
+        mockMvc.perform(put("/users/1/friends/99"))
                 .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void test5_addNormalUser() throws Exception {
+        addUser1(mockMvc);
+    }
+
+    @Test
+    void test6_addFriend() throws Exception {
+        addUser1(mockMvc);
+        addUser2(mockMvc);
+
+        mockMvc.perform(put("/users/1/friends/2")
+        ).andExpect(status().is2xxSuccessful());
+    }
+
+    void addUser1(MockMvc mockMvc) throws Exception {
+        String content = "{\n" +
+                "  \"login\": \"dolore\",\n" +
+                "  \"name\": \"Nick Name\",\n" +
+                "  \"email\": \"mail@mail.ru\",\n" +
+                "  \"birthday\": \"1946-08-20\"\n" +
+                "}";
+        mockMvc.perform(post("/users")
+                        .header("Content-Type", "application/json")
+                        .content(content))
+                .andExpect(status().isOk());
+    }
+
+    void addUser2(MockMvc mockMvc) throws Exception {
+        String content = "{\n" +
+                "  \"login\": \"dolore2\",\n" +
+                "  \"name\": \"Nick Name\",\n" +
+                "  \"email\": \"mail1@mail.ru\",\n" +
+                "  \"birthday\": \"1970-08-20\"\n" +
+                "}";
+        mockMvc.perform(post("/users")
+                        .header("Content-Type", "application/json")
+                        .content(content))
+                .andExpect(status().isOk());
     }
 
 }
